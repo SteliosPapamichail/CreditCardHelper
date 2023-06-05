9684d1fca79e3664f5897cf767e4496d2ef42867
Stelios Papamichail <steliospapamichail@protonmail.com>
2023-06-05 18:56:36 +0300

    - Updated AGP version - Updated gradle dependencies - Disabled clearText traffic in the sample app - Implemented the ability to mask the first 12 digits of a card number using a given string as the mask. - Upgrade to the Compose BOM for dependency management

--- CreditCardHelper/src/main/java/com/steliospapamichail/creditcardmasker/viewtransformations/CardNumberMask.kt
+++ CreditCardHelper/src/main/java/com/steliospapamichail/creditcardmasker/viewtransformations/CardNumberMask.kt
@@ -5,35 +5,57 @@ import androidx.compose.ui.text.input.OffsetMapping
 import androidx.compose.ui.text.input.TransformedText
 import androidx.compose.ui.text.input.VisualTransformation
 
-class CardNumberMask(private val separator: String = " ") : VisualTransformation {
+private const val MAX_CREDIT_CARD_NUMBER_LENGTH = 16
+
+/**
+ * @param separator The separator to use when breaking up a card number in groups of four
+ * @param displayOnlyLastFourDigits When set to true, the first twelve (12) numbers will be masked using the
+ * digitMask param.
+ * @param digitMask The string to use as a mask when displayOnlyLastFourDigits is set to true.
+ * @author Stelios Papamichail
+ */
+class CardNumberMask(
+    private val separator: String = " ",
+    private val displayOnlyLastFourDigits: Boolean = false,
+    private val digitMask: String = "X",
+) : VisualTransformation {
     override fun filter(text: AnnotatedString): TransformedText {
-        return makeCardNumberFilter(text, separator)
+        return makeCardNumberFilter(text, separator, displayOnlyLastFourDigits)
     }
 
-    private fun makeCardNumberFilter(text: AnnotatedString, separator: String): TransformedText {
+    private fun makeCardNumberFilter(
+        text: AnnotatedString,
+        separator: String,
+        displayOnlyLastFourDigits: Boolean
+    ): TransformedText {
         // format: XXXX XXXX XXXX XXXX by default
-        val trimmed = if (text.text.length >= 16) text.text.substring(0..15) else text.text
+        val trimmed =
+            if (text.text.length >= MAX_CREDIT_CARD_NUMBER_LENGTH) text.text.substring(0..15) else text.text
         var out = ""
         for (i in trimmed.indices) {
-            out += trimmed[i]
+            out += if (displayOnlyLastFourDigits && i !in 12..16) digitMask else trimmed[i]
             if (i == 3 || i == 7 || i == 11) out += separator
         }
 
         val offsetMapping = object : OffsetMapping {
             override fun originalToTransformed(offset: Int): Int {
-                return if (offset <= 3) offset
-                else if (offset <= 7) offset + 1
-                else if (offset <= 11) offset + 2
-                else if (offset <= 16) offset + 3
-                else 19
+                return when {
+                    offset <= 3 -> offset
+                    offset <= 7 -> offset + 1
+                    offset <= 11 -> offset + 2
+                    offset <= 16 -> offset + 3
+                    else -> 19
+                }
             }
 
             override fun transformedToOriginal(offset: Int): Int {
-                return if (offset <= 4) offset
-                else if (offset <= 9) offset - 1
-                else if (offset <= 14) offset - 2
-                else if (offset <= 19) offset - 3
-                else 16
+                return when {
+                    offset <= 4 -> offset
+                    offset <= 9 -> offset - 1
+                    offset <= 14 -> offset - 2
+                    offset <= 19 -> offset - 3
+                    else -> 16
+                }
             }
         }
 
